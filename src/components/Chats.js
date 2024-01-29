653b7a59b35f9da7c40d2656ab32f3cb48485c4a
priyampinju <pinjupriyam1826@gmail.com>
2024-01-29 16:49:29 +0530

    hdbfbd

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -10,19 +10,39 @@ const Chats = () => {
                 <p>Search 172 Matches</p>
             </div>
             <div className="new_matches">
-                Corousel
+                <h4>New Matches</h4>
             </div>
-            <h3>Messages</h3>
+            <h4 className='chats_messages'>Messages</h4>
             <div className="chats">
-                <Chat name="Priya"
+                <Chat name="Angel Priya"
                     message='Hi Pin'
                     timestamp='3 minutes ago'
                     profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDYTPxn6uliNMLRejflfqqnqtrYi6WDEGCW6lCShU1jg&s'
                 />
-                <Chat name='Hema'
+                <Chat name='Matha'
                     message='Oi !'
                     timestamp='2 hours ago'
                     profilePic='https://media.istockphoto.com/id/1197578214/photo/beautiful-young-woman.jpg?s=612x612&w=0&k=20&c=XdV1GLQalvNSXKsBv4C0vRDjPfiBOArH6BC_iCFtchg='
+                />
+                <Chat name='Ali'
+                    message='Mashallah'
+                    timestamp='5 hours ago'
+                    profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbJOdBLR_eNIfwt4-eoPsDZNhFXzoQkbPnuw&usqp=CAU'
+                />
+                <Chat name='Shizuka'
+                    message='Konnichiwa'
+                    timestamp='5 hours ago'
+                    profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbJOdBLR_eNIfwt4-eoPsDZNhFXzoQkbPnuw&usqp=CAU'
+                />
+                <Chat name='Khukhbu'
+                    message='Hey Bro!'
+                    timestamp='6 hours ago'
+                    profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbJOdBLR_eNIfwt4-eoPsDZNhFXzoQkbPnuw&usqp=CAU'
+                />
+                <Chat name='Munu'
+                    message='Meaw'
+                    timestamp='6 hours ago'
+                    profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbJOdBLR_eNIfwt4-eoPsDZNhFXzoQkbPnuw&usqp=CAU'
                 />
             </div>
 
